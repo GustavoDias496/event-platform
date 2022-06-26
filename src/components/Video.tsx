c3d77922b994e64b48cc0d861b93e1f47294d729
GustavoDias496 <88061007+GustavoDias496@users.noreply.github.com>
2022-06-26 13:35:22 -0300

    Final commit

--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -1,38 +1,8 @@
 import { DefaultUi, Player, Youtube } from "@vime/react";
-import { gql, useQuery } from "@apollo/client";
 import { CaretRight, DiscordLogo, FileArrowDown, Lightning } from "phosphor-react";
 
 import '@vime/core/themes/default.css'
-
-
-const GET_LESSON_BY_SLUG_QUERY = gql`
-    query GetLessonBySlug ($slug: String) {
-  lesson(where: {slug: $slug}) {
-    title
-    videoId
-    description
-    teacher {
-      bio
-      name
-      avatarURL
-    }
-  }
-}
-`
-
-interface GetLessonBySlugResponde{
-    lesson: {
-        title: string;
-        videoId: string;
-        description: string;
-        teacher: {
-            bio: string;
-            avatarURL: string;
-            name: string;
-        }
-    }
-}
-
+import { useGetLessonBySlugQuery } from "../graphql/generated";
 
 interface VideoProps{
     lessonSlug: string;
@@ -42,13 +12,13 @@ interface VideoProps{
 
 export function Video(props: VideoProps){
 
-    const {data} = useQuery<GetLessonBySlugResponde>(GET_LESSON_BY_SLUG_QUERY, {
+    const {data} = useGetLessonBySlugQuery({
         variables:{
             slug: props.lessonSlug,
         }
     })
 
-    if(!data){
+    if(!data || !data.lesson){
         return(
             <div className="flex-1">
                 <p>Carregando ...</p>
@@ -77,7 +47,8 @@ export function Video(props: VideoProps){
                         {data.lesson.description}
                         </p>
 
-                        <div className="flex items-center gap-4 mt-6">
+                        {data.lesson.teacher && (
+                            <div className="flex items-center gap-4 mt-6">
                             <img 
                                 className="h-16 w-16 rounded-full border-2 border-blue-500"
                                 src={data.lesson.teacher.avatarURL} 
@@ -88,6 +59,7 @@ export function Video(props: VideoProps){
                                 <span className="text-gray-200 text-sm block">CTO @Rocketseat</span>
                             </div>
                         </div>
+                        )}
 
                     </div>
                     <div className="flex flex-col gap-4">
